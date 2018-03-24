0e067ae77308e802b29213de539c21d7ef14f5ab
Quiroz-Rodriguez-David <brigael.bmp@gmail.com>
2018-03-23 20:32:43 -0600

    Clase Coladera Eratostenes
    Codigo fuente de la coladera de eratostenes o criba de eratostenes

---
+++ Col_Eratostenes.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Coladera_de_Eratostenes
+{
+    class Col_Eratostenes
+    {
+        private int _Numero; // número máximo hasta el que se quiere calcular
+        public Col_Eratostenes(int numero) // numero es el limitepara hacer comparaciones 
+        {
+            _Numero = numero;
+        }
+        public string NumPrimos()
+        {
+            string Resultado=" "; // 
+            int n = _Numero;
+            bool[] marcados = new bool[n + 1]; // se crea un vector boleano para darnos los numeros marcado que sean muliplos de los primos con falso y verdadero
+              for (int i = 2; i <= n; i++) // se inicia el ciclo apartir de 2 ya que es el primer numero primo
+               {
+                   if (!marcados[i]) // si los numeros marcados son diferentes de i
+                       for (int j = i * 2; j <= n; j += i) // este contador nos ayudara para saber los multiplos de los numeros primos para descartarlos
+                        marcados[j] = true; // se marcan los que se sabe que NO son primos
+               }
+                for (int i = 2; i < marcados.Length; i++) //lo utilizamos para checar los numeros que quedaron en el vector
+                    if (marcados[i] == false) // se muestran los numeros no marcados y esos son los primos
+                    {
+                        Resultado += i+Environment.NewLine;  
+                    }
+            return Resultado; // regresa los numeros primos 2,3,5, etc
+        }
+        public override string ToString() // nos dara los valores cuando lo invoquemos en el boton
+        {
+            return NumPrimos()+"";
+        }
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Coladera_de_Eratostenes
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Col_Eratostenes coladera = new Col_Eratostenes(1000);
+            ListaNumPrimos.Text += coladera.ToString();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+        }
+    }
+}
